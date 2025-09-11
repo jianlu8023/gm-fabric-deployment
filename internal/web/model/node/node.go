a9402971b68f00a9829587db1ec5e417d2b7be21
jianlu8023 <jianlu8023@gmail.com>
2025-09-11 16:05:45 +0800

    新建本地mysql数据库并使用 调整node_id的字段类型

--- internal/web/model/node/node.go
+++ internal/web/model/node/node.go
@@ -13,7 +13,7 @@ const (
 // Info 节点信息 指的是libp2p节点信息 以libp2p节点当作机器
 type Info struct {
 	AutoUid              int          `json:"auto_uid,omitempty" yaml:"auto_uid,omitempty" gorm:"column:auto_uid;primary_key;auto_increment;"`                                 // 自增ID
-	NodeId               string       `json:"node_id,omitempty" yaml:"node_id,omitempty" gorm:"column:node_id;type:varchar(32);unique;"`                                       // 节点ID
+	NodeId               string       `json:"node_id,omitempty" yaml:"node_id,omitempty" gorm:"column:node_id;type:varchar(256);unique;"`                                      // 节点ID
 	LastAliveMessageTime time.Time    `json:"last_alive_message_time,omitempty" yaml:"last_alive_message_time,omitempty" gorm:"column:last_alive_message_time;type:datetime;"` // 最后一次收到心跳时间
 	IsAlive              sql.NullBool `json:"is_alive,omitempty" yaml:"is_alive,omitempty" gorm:"column:is_alive;type:bool;"`                                                  // 是否存活
 	IsMySelf             sql.NullBool `json:"is_my_self,omitempty" yaml:"is_my_self,omitempty" gorm:"column:is_my_self;type:bool;default:false"`                               // 是否是本机
